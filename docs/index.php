95f671f790aeb0f22c1951715070f411291a6d19
PreRecorded <38794060+PreRecorded@users.noreply.github.com>
2021-06-17 18:54:19 +0200

    Add files via upload

---
+++ docs/index.php
@@ -0,0 +1,16 @@
+<?php
+include("./parts/header.php");
+?>
+    <ul>
+        <li><a href="#"><i class="fas fa-home"></i></a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Help</a></li>
+    </ul>
+    <h1 class="mainTitle">Capchar Technologies</h1>
+ 
+
+<?php
+include("./parts/footer.php");
+?>
+    
